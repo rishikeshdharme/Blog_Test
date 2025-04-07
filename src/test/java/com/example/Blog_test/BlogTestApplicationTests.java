c52353268d3637e618873092e783eb60a7665bd6
RISHIKESH DHARME <145563510+rishikeshdharme@users.noreply.github.com>
2025-04-08 01:18:25 +0530

    Setup the project

---
+++ src/test/java/com/example/Blog_test/BlogTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Blog_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
